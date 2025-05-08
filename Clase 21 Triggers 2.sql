a6210185940eb66d0f3a938d8f4d33f4c06795cb
Smillan94 <sebastian.ma.6@gmail.com>
2025-05-08 16:09:53 +0200

    Clase 21

---
+++ Clase 21 Triggers 2.sql
@@ -0,0 +1,8 @@
+drop trigger tr_dept_control_localidades
+drop trigger tr_dept_before_update;
+drop trigger tr_dept_before_delete;
+drop trigger tr_doctor_control_salario_update;
+
+
+--ejemplo integridad relacional con update
+
